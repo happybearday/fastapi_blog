7f8632045ad6c0833ce0951e38aac5661b939853
HappyBearDay <45377518+happybearday@users.noreply.github.com>
2021-10-27 18:35:11 +0200

    store blog to db

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,25 @@
 import uvicorn
-from fastapi import FastAPI
-
-from blog.schemas import Blog
+from fastapi import FastAPI, Depends
 from . import models, schemas
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
-
 models.Base.metadata.create_all(engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post("/blog")
-def create(blog: Blog):
-    return blog
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
