213d64424eb0db15e921581d0d9cf0248acca5b1
HappyBearDay <45377518+happybearday@users.noreply.github.com>
2021-11-11 09:29:49 +0100

    Add show user

--- blog/main.py
+++ blog/main.py
@@ -68,7 +68,7 @@ def show(id: int, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post("/user")
+@app.post("/user", status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(email=request.email,
                            name=request.name, password=Hash.bcrypt(request.password))
@@ -76,3 +76,19 @@ def create(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/user", status_code=status.HTTP_200_OK, response_model=List[schemas.ShowUser])
+def all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@app.get("/user/{id}", status_code=200, response_model=schemas.ShowUser)
+def show_users(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id {id} is not available")
+    return user
+
